ebf319e01668b275dc6eca393a55e2860561650b
Jmac4Life <143218795+Jmac4Life@users.noreply.github.com>
2023-10-12 23:41:32 -0400

    Arrays (9:53)

--- Game Maker/Assets/Scripts/QuestionSO.cs
+++ Game Maker/Assets/Scripts/QuestionSO.cs
@@ -7,9 +7,21 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField] string strQuestion = "Enter new question text here";
+    [SerializeField] string[] strAnswers = new string[4];
+    [SerializeField] int intCorrectAnswerIndex;
 
     public string StrGetQuestion()
     {
         return strQuestion;
+    }
+
+    public string StrGetAnswer(int intIndex)
+    {
+        return strAnswers[intIndex];
+    }
+
+    public int IntGetCorrectAnswerIndex()
+    {
+        return intCorrectAnswerIndex;
     }
 }
