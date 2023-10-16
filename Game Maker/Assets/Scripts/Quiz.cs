0d9b6efdc2d8ae917fe4261fca50949dff88dc6b
Jmac4Life <143218795+Jmac4Life@users.noreply.github.com>
2023-10-16 13:49:18 -0400

    Swapping Sprites (11:20)

--- Game Maker/Assets/Scripts/Quiz.cs
+++ Game Maker/Assets/Scripts/Quiz.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    int intCorrectAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,27 @@ public class Quiz : MonoBehaviour
         buttonText.text = question.StrGetAnswer(i);
         }
 
+    }
+
+    public void OnAnswerSelected(int intIndex)
+    {
+
+        Image buttonImage;   
+
+        if(intIndex == question.IntGetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            buttonImage = answerButtons[intIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            intCorrectAnswerIndex = question.IntGetCorrectAnswerIndex();
+            string strCorrectAnswer = question.StrGetAnswer(intCorrectAnswerIndex);
+            questionText.text = "Sorry, the correct answer was;\n" + strCorrectAnswer;
+            buttonImage = answerButtons[intCorrectAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
     }
 
 }
